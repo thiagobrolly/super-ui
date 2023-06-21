b482befe49628e5580f1fec62b47c66d5d19d659
Thiago Brolly <thiagoex377@gmail.com>
2023-06-21 16:18:08 -0300

    ci: add docs workflow

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -17,5 +17,12 @@ const config = {
   docs: {
     autodocs: true,
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/super-ui/'
+    }
+
+    return config
+  }
 };
 export default config;
